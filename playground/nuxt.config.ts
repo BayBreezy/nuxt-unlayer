b4c1d67b1ec4d0612bff1a74c9a1f8b2b61273e0
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-10-08 07:58:20 -0500

    docs: add ui-thing to the docs

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,37 @@
 export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
   devtools: { enabled: true, telemetry: false },
-  modules: ["../src/module"],
+
+  modules: [
+    "../src/module",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+  ],
+
   css: ["@/assets/main.css"],
   compatibilityDate: "2024-10-08",
+
+  tailwindcss: {
+    exposeConfig: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
 });
